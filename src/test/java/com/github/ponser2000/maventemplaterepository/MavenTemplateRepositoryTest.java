8b7b8f9f2735698341709a65730bac865f064734
Sergey <ponser2000@gmail.com>
2021-09-06 14:05:02 +0300

    Initial commit

---
+++ src/test/java/com/github/ponser2000/maventemplaterepository/MavenTemplateRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.github.ponser2000.maventemplaterepository;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit-level testing for {@link MavenTemplateRepository} object.
+ */
+public class MavenTemplateRepositoryTest {
+
+    @Test
+    public void shouldCreateJavaRepositoryTemplateMain() {
+        MavenTemplateRepository main = new MavenTemplateRepository();
+        Assertions.assertNotNull(main);
+    }
+
+}
